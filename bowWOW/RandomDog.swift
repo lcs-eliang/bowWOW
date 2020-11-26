d49d3180503d83c856da900b09a13ad67b344f58
Emily Liang <eliang@lcs.on.ca>
2020-11-26 09:48:39 -0500

    Finished bow WOW.

---
+++ bowWOW/RandomDog.swift
@@ -0,0 +1,13 @@
+//
+//  RandomDog.swift
+//  bowWOW
+//
+//  Created by Emily Liang on 2020-11-26.
+//
+
+import Foundation
+
+struct RandomDog: Codable {
+    var message: String
+    var status: String
+}
