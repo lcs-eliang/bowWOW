6fd51a8aa27bb0c3525e782a8920ef30a13621a2
Emily Liang <eliang@lcs.on.ca>
2020-11-26 08:56:52 -0500

    Initial Commit

---
+++ bowWOW/bowWOWApp.swift
@@ -0,0 +1,17 @@
+//
+//  bowWOWApp.swift
+//  bowWOW
+//
+//  Created by Emily Liang on 2020-11-26.
+//
+
+import SwiftUI
+
+@main
+struct bowWOWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
